fa11a809454966b0f7ece8435b497aad0d4b8d64
Frede-72 <frjo0005@stud.ek.dk>
2026-08-28 10:33:01 +0200

    variables_1.js added

--- 01._Miscellaneous/variables_1.js
+++ 01._Miscellaneous/variables_1.js
@@ -1,10 +1,52 @@
-//node REPL (read-evaluate-print loop)
-//Type coercion
+// node REPL (read-evaluate-print loop)
+// Type coercion
 
-//console.log("ee");
+// console.log("ee");
 
-//Use const when possible else let
+// Use const when possible else let
+// Use === and !==
 
 const number = 111;
 
-//node <filename>
+// const is constant in assignment
+
+const person = {
+  // key-value pair
+  name: "Bob"
+};
+
+person.age = 99;
+
+console.log(person);
+
+// delete person.name;
+
+// const person2;
+
+console.log(person);
+
+const things = ["mouse"];
+things.push("car");
+console.log(things);
+
+// things.pop();
+
+// Use , in console.log. +(concatenate) might change values
+
+console.log(things[0], things[1]);
+
+// Data types
+// Strings, Boolean, Number, BigInt, null, undefined, Object, Symbol,
+
+// node <filename>
+
+const greeting = "Hej 'e'";
+const greetingTwo = 'Hej """""e"';
+const greetingThree = `Hej, 'e' "${person.name}"
+
+ee
+`;
+
+console.log(greeting);
+console.log(greetingTwo);
+console.log(greetingThree);
